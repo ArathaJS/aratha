5f584be95620947e0cc67f24819093931efb6499
Mak Nazecic-Andrlon <mnazecic@student.unimelb.edu.au>
2017-03-13 19:29:55 +1100

    Add concolic testing loop and interface to Z3
    This also adds a simple S-expression parser intended for SMT-LIB
    output.

--- analysis.js
+++ analysis.js
@@ -1,25 +1,20 @@
 (function(sandbox) {
+    "use strict";
+
     var util = require('util');
+    var child_process = require('child_process');
     var _ = require('lodash');
+    const EventEmitter = require('events');
+    const fs = require('fs');
 
     var branches = {};
+    var inputs = {};
     var constraints = [];
-    var varNameCounter = 0;
 
     function addConstraint(val) {
         if (!_.some(constraints, _.partial(_.isEqual, val))) {
             constraints.push(val);
         }
-    }
-
-    function collectVariables(expr) {
-        var variables = {};
-        expr.visit(function(expr) {
-            if(expr instanceof Variable) {
-                variables[expr.name] = expr;
-            }
-        });
-        return variables;
     }
 
     function stringifySExpression(expr) {
@@ -364,6 +359,175 @@
         return ["js." + this.op, valueToFormula(this.expr)];
     }
 
+    class SExprParser {
+        constructor() {
+            this._levels = [];
+            this._curtok = null;
+            this._parsingString = false;
+            this._numQuotes = 0;
+        }
+
+        parse(str, cb) {
+            for(let i = 0; i < str.length; ++i) {
+                let expr = this._feedChar(str[i]);
+                if (expr !== null && this._levels.length === 0) {
+                    cb(expr);
+                }
+            }
+        }
+
+        _feedChar(char) {
+            if (this._parsingString) {
+                if (char === '"') {
+                    this._numQuotes++;
+
+                    if (this._numQuotes >= 2) {
+                        this._numQuotes -= 2;
+                        this._curtok += '"';
+                    }
+
+                    return null;
+                } else {
+                    if (this._numQuotes === 0) {
+                        this._curtok += char;
+                        return null;
+                    } else if (this._numQuotes === 1) {
+                        this._parsingString = false;
+                        this._numQuotes = 0;
+                        this._finishToken();
+                    } else {
+                        throw new Error("impossible");
+                    }
+                }
+            }
+
+            switch(char) {
+                case "\r":
+                case "\n":
+                case "\t":
+                case " ":
+                    return this._finishToken();
+                case "(":
+                    this._levels.push([]);
+                    break;
+                case ")":
+                    if (this._levels.length >= 1) {
+                        this._finishToken();
+                        let expr = this._levels.pop();
+                        if (this._levels.length > 0) {
+                            _.last(this._levels).push(expr);
+                        } else {
+                            return expr;
+                        }
+                    } else {
+                        throw new Error("parse error: too many close-parens");
+                    }
+                    break;
+                case '"':
+                    this._parsingString = true;
+                    this._curtok = "";
+                    break;
+                default:
+                    if (this._curtok === null) {
+                        this._curtok = "";
+                    }
+                    this._curtok += char;
+                    break;
+            }
+
+            return null;
+        }
+
+        _finishToken() {
+            let token = this._curtok;
+            if (token === null)
+                return null;
+            this._curtok = null;
+
+            if (this._levels.length > 0) {
+                _.last(this._levels).push(token);
+                return null;
+            } else {
+                return token;
+            }
+        }
+    }
+
+    function collectVariables(expr) {
+        var variables = {};
+        expr.visit(function(expr) {
+            if(expr instanceof Variable) {
+                variables[expr.name] = expr;
+            }
+        });
+        return variables;
+    }
+
+    function generateSolverCommands(constraints) {
+        var commands = [];
+        var variables = {};
+
+        for (var i = 0; i < constraints.length; ++i) {
+            Object.assign(variables, collectVariables(constraints[i]));
+        }
+
+        _.forEach(variables, function(v) {
+            commands.push(v.declarationFormula());
+            if (!v.type.trivial()) {
+                commands.push(["assert", v.type.constraintFor(v)]);
+            }
+        });
+
+        for (var i = 0; i < constraints.length; ++i) {
+            commands.push(["assert", constraints[i].toFormula()]);
+        }
+
+        commands.push(["check-sat"]);
+        commands.push(["get-value", _.map(variables, function(v) { return v.toFormula(); })]);
+        return commands;
+    }
+
+    const MAX_INPUTS = 10;
+    const SOLVER_PATH = '../../z3/z3-4.5.0-x64-win/bin/z3';
+    const ES_THEORY_PATH = 'ecmascript.smt2';
+
+    class Z3Solver extends EventEmitter {
+        constructor() {
+            super();
+
+            let z3 = child_process.spawn(SOLVER_PATH, ['-smt2', '-in']);
+            z3.stdin.write(fs.readFileSync(ES_THEORY_PATH));
+
+            z3.stdout.setEncoding('utf8');
+
+            let parser = new SExprParser();
+            z3.stdout.on('data', (data) => {
+                parser.parse(data, (expr) => {
+                    this.emit('output', expr);
+                });
+            });
+
+            this.process = z3;
+        }
+
+        push(n) {
+            this.submitCommands([["push", n.toString()]]);
+        }
+
+        pop(n) {
+            this.submitCommands([["pop", n.toString()]]);
+        }
+
+        submitCommands(commands) {
+            var commandString = _.map(commands, stringifySExpression).join("\n");
+            console.log(commandString);
+            this.process.stdin.write(commandString);
+        }
+
+        close() {
+            this.process.stdin.end("(exit)");
+        }
+    }
 
     J$.analysis = {
 
@@ -386,42 +550,6 @@
         }
     },
 
-     /**
-      * This callback is called when an execution terminates in node.js.  In a browser
-      * environment, the callback is called if ChainedAnalyses.js or ChainedAnalysesNoCheck.js
-      * is used and Alt-Shift-T is pressed.
-      *
-      * @returns {undefined} - Any return value is ignored
-      */
-      endExecution : function () {
-        // console.log(util.inspect(constraints, false, null));
-        var commands = [];
-        var variables = {};
-
-        for (var i = 0; i < constraints.length; ++i) {
-            Object.assign(variables, collectVariables(constraints[i]));
-        }
-
-        _.forEach(variables, function(v) {
-            commands.push(v.declarationFormula());
-            if (!v.type.trivial()) {
-                commands.push(["assert", v.type.constraintFor(v)]);
-            }
-        });
-
-        for (var i = 0; i < constraints.length; ++i) {
-            commands.push(["assert", constraints[i].toFormula()]);
-        }
-
-        commands.push(["check-sat"]);
-        commands.push(["get-value", _.map(variables, function(v) { return v.toFormula(); })]);
-
-
-        for (var i = 0; i < commands.length; ++i) {
-            console.log(stringifySExpression(commands[i]));
-        }
-    },
-
     binaryPre: function (iid, op, left, right, isOpAssign, isSwitchCaseComparison, isComputed) {
         if ((left instanceof SymbolicValue) || (right instanceof SymbolicValue)) {
             return {op: op, left: left, right: right, skip: true};
@@ -432,11 +560,93 @@
         if ((left instanceof SymbolicValue) || (right instanceof SymbolicValue)) {
             return {result: new Binary(iid, op, left, right)};
         }
+    },
+
+    invokeFunPre: function (iid, f, base, args, isConstructor, isMethod, functionIid, functionSid) {
+        if (f === sandbox.readInput) {
+            return {skip: true, f: f, base: base, args: args};
+        }
+    },
+
+    invokeFun: function (iid, f, base, args, result, isConstructor, isMethod, functionIid, functionSid) {
+        if (f === sandbox.readInput) {
+            //var giid = J$.sid + "_" + iid;
+            var giid = iid;
+            return {result: new Variable("var" + giid, inputs[giid])};
+        }
+    },
+
+    onReady: function(cb) {
+        var input_filename = './example.js';
+        var solver = new Z3Solver();
+        solver.on('output', function(expr) {
+            console.log(expr);
+        });
+
+        function parseVarName(varName) {
+            // Slice off the 'var' prefix.
+            return varName.slice(3);
+        }
+
+        function parseVal(value) {
+            let type = value[0], contents = value[1];
+            switch(type) {
+                case "Num":
+                return parseFloat(contents, 10);
+                default:
+                throw new Error("invalid value type " + type);
+            }
+        }
+
+        function runAnalysis(n) {
+            if (n <= 0) {
+                console.log("finished: reached iteration limit");
+                solver.close();
+                return;
+            }
+
+            delete require.cache[require.resolve(input_filename)];
+            constraints.length = 0;
+            cb();
+
+            if (constraints.length === 0) {
+                console.log("finished: no more constraints to solve");
+                solver.close();
+                return;
+            }
+
+            let commands = generateSolverCommands(constraints);
+
+            solver.push(1)
+            solver.submitCommands(commands);
+            solver.pop(1);
+
+            solver.once('output', (expr) => {
+                if (expr === 'sat') {
+                    solver.once('output', (expr) => {
+                        for(let j = 0; j < expr.length; j++) {
+                            let name = expr[j][0], value = expr[j][1];
+                            inputs[parseVarName(name)] = parseVal(value);
+                        }
+
+                        runAnalysis(n - 1);
+                    });
+                } else if (expr === 'unsat') {
+                    // Absorb the error message from the (get-value) command
+                    // before looping to test again.
+                    solver.once('output', () => runAnalysis(n - 1));
+                } else {
+                    throw new Error(`got ${expr} instead of sat or unsat`);
+                }
+            });
+        }
+
+        runAnalysis(MAX_INPUTS);
     }
 };
 
 sandbox.readInput = function(concreteValue) {
-    return new Variable("var" + varNameCounter++, concreteValue);
+    throw new Error("invalid call of readInput");
 };
 
 }(J$));
--- example.js
+++ example.js
@@ -7,7 +7,7 @@ function bar() {
 }
 
 function program(b) {
-    for (var i = 0; i < 10 && b === b; ++i) {
+    for (var i = 0; i < 10 && b + 1 === 3; ++i) {
         if (i % 2 === 0) {
             foo();
         } else {
