c4680ff945667b228aa6207cd8e9535070329a6c
Mak Nazecic-Andrlon <mnazecic@student.unimelb.edu.au>
2017-04-01 14:59:45 +1100

    Test examples with both CVC4 and Z3

--- test/smt2.js
+++ test/smt2.js
@@ -2,34 +2,24 @@
 
 const child_process = require("child_process");
 const path = require("path");
-const process = require("process");
-
-const CVC4_PATH = process.env.CVC4_PATH;
+const { testCVC4, testZ3 } = require("./testUtils");
 
 describe("running CVC4", function() {
     context("on common.smt2", function() {
+        const cvc4Path = testCVC4();
         it("should terminate with no errors", function(done) {
-            if (CVC4_PATH !== undefined) {
-                const filePath = path.resolve(__dirname, "../lib/smt2/common.smt2");
-                child_process.execFile(CVC4_PATH, ["--lang=smt2", "--strings-exp", "--incremental", filePath], done);
-            } else {
-                this.skip();
-            }
+            const filePath = path.resolve(__dirname, "../lib/smt2/common.smt2");
+            child_process.execFile(cvc4Path, ["--lang=smt2", "--strings-exp", "--incremental", filePath], done);
         });
     });
 });
 
-const Z3_PATH = process.env.Z3_PATH;
-
 describe("running Z3", function() {
     context("on common.smt2", function() {
+        const z3Path = testZ3();
         it("should terminate with no errors", function(done) {
-            if (Z3_PATH !== undefined) {
-                const filePath = path.resolve(__dirname, "../lib/smt2/common.smt2");
-                child_process.execFile(Z3_PATH, [filePath], done);
-            } else {
-                this.skip();
-            }
+            const filePath = path.resolve(__dirname, "../lib/smt2/common.smt2");
+            child_process.execFile(z3Path, [filePath], done);
         });
     });
 });
