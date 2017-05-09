443a02c84231fb209b31ed6bc36cac88c9e63fe8
Mak Nazecic-Andrlon <mnazecic@student.unimelb.edu.au>
2017-05-09 21:32:41 +1000

    Add support for String.prototype.concat()

---
+++ test/examples/example20.js
@@ -0,0 +1,10 @@
+/* global J$ */
+
+var x = J$.readInput(), y = J$.readInput(), z = J$.readInput();
+var r = "";
+
+if (x + y === "a" && r.concat(x, y, z) === "abc") {
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
