1efc51f76937d34af86ccaed9ec2d03330ec8d06
robama <amadini@cs.unibo.it>
2018-07-09 14:12:19 +1000

    Added folder for automated tests.

---
+++ rob_test/test2.js
@@ -0,0 +1,9 @@
+x = J$.readInput();
+y = J$.readInput();
+if (x[y] >= 5)  
+  x = 0;  
+else
+  if (y[x] === "g")
+    x = 2;
+  else
+    x = 1;
---
+++ rob_test/test3.js
@@ -0,0 +1,14 @@
+x = J$.readInput();
+y = J$.readInput();
+if (x + y > -1)
+  x = 0;
+else
+  y = 0;
+  
+//FIXME: Seg. Fault.
+//x = J$.readInput();
+//y = J$.readInput();
+//if (x + y > 0)
+//  x = 0;
+//else
+//  y = 0;
