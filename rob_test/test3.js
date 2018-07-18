81433c3eb247dfc51e14f56bd0f20d0a353fe190
robama <amadini@cs.unibo.it>
2018-07-18 10:45:34 +1000

    Minor.

--- rob_test/test3.js
+++ rob_test/test3.js
@@ -1,14 +1,23 @@
-x = J$.readInput();
-y = J$.readInput();
-if (x + y > 0)
-  console.log("PC1")
-else
-  console.log("PC2")
+//x = J$.readInput();
+//y = J$.readInput();
+//if (x + y > 0)
+//  console.log("PC1")
+//else
+//  console.log("PC2")
   
-//FIXME: Seg. Fault.
+//FIXME 1: Seg. Fault.
 //x = J$.readInput();
 //y = J$.readInput();
 //if (x + y < 0)
-//  x = 0;
+//  console.log("PC1")
 //else
-//  y = 0;
+//  console.log("PC2")
+
+//FIXME 2: mzn2fzn stuck with this model:
+//include "javascript.mzn";
+//int: MIN_LOC;
+//int: N_LOCS;
+//int: N_IMPL_PROPS;
+//int: N_EXPL_PROPS;
+//var string: x = divi("0", "0");
+//solve satisfy;
