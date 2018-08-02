3ac14bef9219141986fb87ecce8aa577bf71ee48
robama <amadini@cs.unibo.it>
2018-08-02 16:53:54 +1000

    Tests automation.

---
+++ run-tests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+DIR=`dirname $(readlink -f "$0")`
+TDIR=$DIR/test/examples
+JAL=$DIR/../src/js/commands/jalangi.js
+N_TESTS=26
+# TODO: Inspect 1, 3, 11, 17.
+COV=(NaN 37.5 75 75 100 100 100 88.89 83.33 100 100 80 100 100 66.67 100 100 \
+     66.67 100 100 100 100 100 100 100 100 100)
+for (( i=1; i<=$N_TESTS; i++ ))
+do
+  j=$TDIR/example"$i".js
+  echo "Testing $j"
+  SOLVER=G-Strings node $JAL --analysis ./ $j >/dev/null 2>/dev/null
+  ilog=`basename $j | awk -F"." '{print $1}'`.inputlog.json
+  mv inputlog.json $ilog
+  echo "===== INPUT ===== "
+  cat $ilog
+  nyc node $DIR/../experiments/bin/replay-inputs.js $j 2>/dev/null | strings | \
+    grep '  '`basename "$j"` | awk -F"|" '{print $5}' | sed 's/ //g' > x
+  cov=`cat x`
+  rm x $ilog
+  echo "Coverage: $cov%"
+  echo "Expected: ${COV[$i]}%"
+  if
+    [[ "$cov" != "${COV[$i]}" ]]
+  then
+    echo "Error!"
+    exit 1
+  else
+    echo -e "All good!\n=========\n"
+  fi
+done
---
+++ test/examples/example26.js
@@ -0,0 +1,9 @@
+var x = J$.readInput();
+var y = J$.readInput();
+var z = J$.readInput();
+if (z === 0 && x != null && y != null && typeof(x) === "object")
+  y = x
+if (x === y)
+  console.log(1)
+else
+  console.log(2)
