27d4b16a4429b18365d2d1ae2888eee180c4ba23
robama <amadini@cs.unibo.it>
2018-09-18 15:00:05 +1000

    Fixed async calls.

--- test/examples/example4.js
+++ test/examples/example4.js
@@ -1,8 +1,8 @@
 /* global J$ */
 
-var x = J$.readInput();
+var x = J$.readNumber(); //J$.readInput();
 
-if (-x === 19844) {
+if (-x === 1984) {
     console.log("success; x: ", x);
 } else {
     console.log("failure: x: ", x);
